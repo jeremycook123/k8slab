478fe89a2048cbab0eda8d9ec0725fec8abb6148
Jeremy Cook <jeremycook123@gmail.com>
2020-12-22 15:01:12 +1300

    added in K8s dashboard

---
+++ l6/code/k8s-dash/install.sh
@@ -0,0 +1,8 @@
+echo installing k8s dashboard...
+helm install k8s-dashboard --namespace monitoring k8s-dashboard/kubernetes-dashboard --set=protocolHttp=true --version 3.0.2
+
+echo exposing dashboard ui...
+kubectl expose deployment k8s-dashboard --type=NodePort --name=k8s-dashboard --port=30990 --target-port=9090 -n monitoring
+kubectl patch service k8s-dashboard -n monitoring -p '{"spec":{"ports":[{"nodePort": 30990, "port": 30990, "protocol": "TCP", "targetPort": 9090}]}}'
+
+echo k8s dashboard install finished!
