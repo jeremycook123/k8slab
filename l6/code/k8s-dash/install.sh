14ed7f2e9415820d12882b9132cf50a0289b47de
Jeremy Cook <jeremycook123@gmail.com>
2020-12-22 15:03:40 +1300

    updated install script

--- l6/code/k8s-dash/install.sh
+++ l6/code/k8s-dash/install.sh
@@ -1,5 +1,7 @@
 echo installing k8s dashboard...
 helm install k8s-dashboard --namespace monitoring k8s-dashboard/kubernetes-dashboard --set=protocolHttp=true --version 3.0.2
+kubectl apply -f /home/project/code/k8s-dash/kubernetes-dashboard-role.yaml
+kubectl apply -f /home/project/code/k8s-dash/kubernetes-dashboard-rolebinding.yaml
 
 echo exposing dashboard ui...
 kubectl expose deployment k8s-dashboard --type=NodePort --name=k8s-dashboard --port=30990 --target-port=9090 -n monitoring
